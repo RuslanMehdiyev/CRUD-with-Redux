8cb317dc5c953705e5561e556173228c9647123a
Ruslan <ruslanmehdiyev922@gmail.com>
2023-01-12 11:18:43 +0400

    finished

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -9,35 +9,41 @@ function Favorites() {
   return (
     <>
       <h2 style={{ textAlign: "center" }}>Favorites List</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Contact Title</th>
-            <th>Contact Name</th>
-            <th>Company Name</th>
-            <th>
-              <button onClick={() => dispatch(deleteAll())}>Delete All</button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {favoritesData &&
-            favoritesData.map((e, key) => (
-              <tr key={key}>
-                <td>{e.id}</td>
-                <td>{e.contactTitle}</td>
-                <td>{e.contactName}</td>
-                <td>{e.companyName}</td>
-                <td>
-                  <button onClick={() => dispatch(deleteFromFav(e))}>
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      {favoritesData.length ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Contact Title</th>
+              <th>Contact Name</th>
+              <th>Company Name</th>
+              <th>
+                <button onClick={() => dispatch(deleteAll())}>
+                  Delete All
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {favoritesData &&
+              favoritesData.map((e, key) => (
+                <tr key={key}>
+                  <td>{e.id}</td>
+                  <td>{e.contactTitle}</td>
+                  <td>{e.contactName}</td>
+                  <td>{e.companyName}</td>
+                  <td>
+                    <button onClick={() => dispatch(deleteFromFav(e))}>
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      ) : (
+        <h3 style={{ textAlign: "center",marginTop:"1rem" }}>List is Empty</h3>
+      )}
     </>
   );
 }
